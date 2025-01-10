a7f852cb5aba7fef4e4cac60409061d4879ca7ec
Ramzesp2009 <ramzesp2009@gmail.com>
2025-01-10 08:27:01 +0100

    Online Store

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Ви увійшли в аккаунт.")
+
+                if request.POST.get("next", None):
+                    return HttpResponseRedirect(request.POST.get("next"))
+
                 return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
@@ -69,6 +73,7 @@ def profile(request):
     return render(request, "users/profile.html", context)
 
 
+@login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Ви успішно вийшли з аккаунту.")
     auth.logout(request)
