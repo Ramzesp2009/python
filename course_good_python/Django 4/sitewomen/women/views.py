f94e0b00af0af28fba33027e8229a120e2af5852
Ramzesp2009 <ramzesp2009@gmail.com>
2025-02-08 13:31:53 +0100

    08.02.2025

--- course_good_python/Django 4/sitewomen/women/views.py
+++ course_good_python/Django 4/sitewomen/women/views.py
@@ -2,51 +2,64 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
-# import requests
 
-manu = ["About this web", "Add an artickle", "Contact us", "Login", "Register"]
 
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+menu = [
+    {'title': "About this web", 'url_name': 'about'},
+    {'title': "Add an article", 'url_name': 'add_page'},
+    {'title': "Contact us", 'url_name': 'contact'},
+    {'title': "Login", 'url_name': 'login'},
+]
+
+data_db = [
+    {'id': 1, 'title': 'Angelina Jolie', 'content': "<h1>Angelina Jolie</h1> born June 4, 1975 is an American actress, "
+                                                    "filmmaker, and humanitarian. The recipient of numerous accolades, "
+                                                    "including an Academy Award, a Tony Award and three Golden Globe Awards,"
+                                                    " she has been named Hollywood's highest-paid actress multiple times.",
+     'is_published': True},
+    {'id': 2, 'title': 'Brad Pitt', 'content': '<h1>Brad Pitt</h1> is an American actor.', 'is_published': False},
+    {'id': 3, 'title': 'Demi Moore', 'content': '<h1>Demi Moore</h1> is an American actress.', 'is_published': True},
+]
+
+cats_db = [
+    {'id': 1, 'name': 'Actress'},
+    {'id': 2, 'name': 'Singer'},
+    {'id': 3, 'name': 'Sportsmans'},
+]
+
 
 def index(request): # HttpRequest
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
+
     data = {'title': "The main page of the site",
-            'manu': manu,
-            'float': 28.56,
-            'lst': [1, 2, 3, 4, 5],
-            'set': {1, 2, 3, 4, 5},
-            'dict': {
-                'key1': 'value1',
-                'key2': 'value2'
-            },
-            'obj': MyClass(10,20),
+            'menu': menu,
+            'posts': data_db,
+            'cat_selected': 0,
             }
     return render(request, 'women/index.html', context=data)
 
 def about(request):
-    return render(request, 'women/about.html', {'title': "About this web"})
+    return render(request, 'women/about.html', {'title': "About this web", 'menu': menu})
 
-def cats(request, cat_id):
-    return HttpResponse(f"<h1>The articles by categories</h1><p>id: {cat_id}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"Displaying the article with id = {post_id}")
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>The articles by categories</h1><p>id: {cat_id}</p>")
+def addpage(request):
+    return HttpResponse("Add page")
 
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>The articles by categories</h1><p>slug: {cat_slug}</p>")
+def contact(request):
+    return HttpResponse("Contact us")
 
-def archive(request, year):
-    if year > 2023:
-        url = reverse('cats', args=('sport', ))
-        return HttpResponsePermanentRedirect(url)
+def login(request):
+    return HttpResponse("Login")
 
-    return HttpResponse(f"<h1>Archive</h1><p>year: {year}</p>")
+def show_category(request, cat_id):
+    data = {
+        'title': "The main page of the site",
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=data)
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Page not found</h1>")
