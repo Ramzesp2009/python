94e93e90041b65394075ffd22d1102d4f20b74aa
ramzesp2009 <ramzesp2000@gmail.com>
2025-03-19 12:31:22 +0100

    new version todoliste

---
+++ gcd.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+def gcd(m,n):
+    if m == n:
+        return m
+
+    smaller = m if m < n else n
+    greater = m if m > n else n
+
+    max_divisor = greater / 2 if greater / 2 > smaller else smaller
+
+    for i in range(max_divisor, 0, -1):
+        if m % i == 0 and n % i == 0:
+            return i
+    return 1
+
+if __name__ == "__main__":
+    print(gcd(int(input("Enter the first number: ")), int(input("Enter the second number: "))))
